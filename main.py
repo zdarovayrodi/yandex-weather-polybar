d01261545108edda3c0e1468f1cf05d342099965
zdarovayrodi <zdarovayrodi@icloud.com>
2023-01-03 22:05:47 +0300

    feat: add base

---
+++ main.py
@@ -0,0 +1,35 @@
+import requests
+
+# send request to yandex weather api
+TOKEN = 'your token'
+LAT = 'your latitude'
+LON = 'your longitude'
+
+# example of request from docs
+# https://yandex.ru/dev/weather/doc/dg/concepts/forecast-info.html
+'''
+GET https://api.weather.yandex.ru/v2/informers?
+ lat=<широта>
+ & lon=<долгота>
+ & [lang=<язык ответа>]
+
+X-Yandex-API-Key: <значение ключа>
+'''
+
+# response
+try:
+    response = requests.get('https://api.weather.yandex.ru/v2/informers?lat={}&lon={}&lang={}'.format(LAT, LON, LANG),
+        headers={'X-Yandex-API-Key': TOKEN}).json()
+        
+    # print what you want to see
+    TEMP_REAL = response['fact']['temp']
+    TEMP_FEEL = response['fact']['feels_like']
+        
+    # in condition i dont want - and _ symbols
+    CONDITION = response['fact']['condition'].replace('-', ' ').replace('_', ' ')
+
+    # output
+    print(TEMP_REAL, TEMP_FEEL, CONDITION)
+except:
+    exit()
+
